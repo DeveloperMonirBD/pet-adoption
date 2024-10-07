c84f0c5c5881c61215e38fb27a9215d527d08389
Monir <mrmonir0558@gmail.com>
2024-10-07 13:41:00 +0600

    details button functionnality done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // **load 4pets btn fetch
 const leadCategoriesBtn = async () => {
     try {
@@ -9,25 +10,32 @@ const leadCategoriesBtn = async () => {
     }
 };
 
+ // ** remove active class
+const removeActiveClass = () => {
+    const buttons = document.getElementsByClassName('category-btn');
+    for (let btn of buttons) {
+        btn.classList.remove('active');
+    }
+};
+
 // ** load pets btn functionality
 const loadCategoryPets = (id) => {
     // alert(id)
     fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
         .then(res => res.json())
-        .then(data => displayAllPets(data.data))
+      .then(data => {
+        // active class remove 
+        removeActiveClass();
+
+        // active class add
+        const activeBtn = document.getElementById(`btn-${id}`)
+        activeBtn.classList.add("active")
+          displayAllPets(data.data);
+        })
         .catch(error => console.log(error));
 };
 
-// ** spinner not working------------
-const handleSearch = () => {
-    const spinner = document.getElementById('spinner');
-    spinner.style.display = 'block';
-    spinner.style.display = 'flex';
 
-    setTimeout(function () {
-        loadCategoryPets();
-    }, 3000);
-};
 
 // **load 4pets btn display
 const displayCategoriesBtn = items => {
@@ -37,8 +45,9 @@ const displayCategoriesBtn = items => {
         const buttonContainer = document.createElement('div');
         buttonContainer.innerHTML = `
     <button
+        id = "btn-${category}"
         onclick="loadCategoryPets('${category}')"
-        class="border w-full block lg:inline-block px-16 py-6 rounded-xl hover:rounded-full border-[#bae8ec] hover:bg-[#bae8ec] hover:border-[#0E7A81] text-xl font-bold  transition-all">
+        class="category-btn border w-full block lg:inline-block px-16 py-6 rounded-xl hover:rounded-full border-[#bae8ec] hover:bg-[#bae8ec] hover:border-[#0E7A81] text-xl font-bold  transition-all">
         <div class="flex justify-center items-center gap-4" href="">
           <img src=${category_icon} alt="" />
           <p>${category}</p>
@@ -50,7 +59,7 @@ const displayCategoriesBtn = items => {
     });
 };
 
-// ** display all pets
+// ** display all pets fetch
 const loadAllPets = async () => {
     try {
         const response = await fetch(`https://openapi.programming-hero.com/api/peddy/pets`);
@@ -61,11 +70,66 @@ const loadAllPets = async () => {
     }
 };
 
+// ** load details
+const loadDetails = async(petId) => {
+  console.log(petId);
+  const url = `https://openapi.programming-hero.com/api/peddy/pet/${petId}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayDetails(data.petData);
+};
+
+// ** display details 
+const displayDetails = (petItem) => {
+  const detailsContainer = document.getElementById('modal-content');
+
+  const { petId, pet_details, image, category, breed, date_of_birth, gender, price } = petItem;
+
+  detailsContainer.innerHTML = `
+    <div class="card bg-base-100">
+        <figure>
+          <img class="w-full" src=${image}
+            class="rounded-xl" />
+        </figure>
+        <div class="pt-4 space-y-2">
+          <h3 class="card-title font-bold">${category ? category : 'Data not found'}</h3>
+
+          <div class=" pb-4 space-y-2">
+            <div class="flex items-center gap-2 text-gray-500">
+              <img src="./images/breed.png" alt="" />
+              <p>Breed: ${breed ? breed : 'Data not found'}</p>
+            </div>
+            <div class="flex items-center gap-2 text-gray-500">
+              <img src="./images/birth.png" alt="" />
+              <p>Birth: ${date_of_birth ? date_of_birth : 'Data not found'}</p>
+            </div>
+            <div class="flex items-center gap-2 text-gray-500">
+              <img src="./images/gender.png" alt="" />
+              <p>Gender: ${gender ? gender : 'Data not found'}</p>
+            </div>
+            <div class="flex items-center gap-2 text-gray-500">
+              <img src="./images/dolor.png" alt="" />
+              <p>Price: ${price ? price + '$' : 'Data not found'}</p>
+            </div>
+
+            <div class="mt-4">
+              <h3 class="font-bold text-xl mb-2">Details Information</h3>
+              <p>${pet_details ? pet_details : 'Data not found'}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+  document.getElementById('customModal').showModal();
+}
+
+
+
+
 // ** display all pets
 const displayAllPets = pets => {
   const cardContainer = document.getElementById('card-container');
   cardContainer.innerHTML = '';
-
   if (pets.length === 0) {
     cardContainer.innerHTML = `
     <div class= "min-h-screen">
@@ -83,9 +147,8 @@ const displayAllPets = pets => {
     `;
     return
   }
-
     pets.forEach(pet => {
-        const { image, category, breed, date_of_birth, gender, price } = pet;
+        const {petId, pet_details, image, category, breed, date_of_birth, gender, price } = pet;
         const petDiv = document.createElement('div');
         petDiv.innerHTML = `
         <div class="card bg-base-100 shadow-xl p-6 border">
@@ -118,68 +181,16 @@ const displayAllPets = pets => {
 
       
             <div class="grid grid-cols-3 gap-4 px-4 pt-2">
-              <button class="btn text-lg p-2 border-slate-300 hover:border-slate-300">
+              <button class="btn text-md border-[#0E7A81] hover:bg-[#bae8ec] hover:border-[#0E7A81]  transition-all">
                 <i class="fa-regular fa-thumbs-up"></i>
               </button>
-              <button class="btn text-md border-slate-300 hover:border-slate-400">
+
+              <button class="btn text-md border-[#0E7A81] hover:bg-[#bae8ec] hover:border-[#0E7A81]  transition-all">
                 Adopt
               </button>
 
-
-              <button class="btn text-md border-slate-300 hover:border-slate-300"
-                onclick="my_modal_1.showModal()">Details</button>
-
-              <dialog id="my_modal_1" class="modal">
-                <div class="modal-box">
-
-                  <div class="card bg-base-100">
-                    <figure>
-                      <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes"
-                        class="rounded-xl" />
-                    </figure>
-                    <div class="pt-4 space-y-2">
-                      <h3 class="card-title font-bold">Mister Tartosh</h3>
-
-                      <div class=" pb-4 space-y-2">
-                        <div class="flex items-center gap-2 text-gray-500">
-                          <img src="./images/breed.png" alt="" />
-                          <p>Breed: Golder Retriver</p>
-                        </div>
-                        <div class="flex items-center gap-2 text-gray-500">
-                          <img src="./images/birth.png" alt="" />
-                          <p>Birth: 2024</p>
-                        </div>
-                        <div class="flex items-center gap-2 text-gray-500">
-                          <img src="./images/gender.png" alt="" />
-                          <p>Gender: Female</p>
-                        </div>
-                        <div class="flex items-center gap-2 text-gray-500">
-                          <img src="./images/dolor.png" alt="" />
-                          <p>Price: 199$</p>
-                        </div>
-
-                        <div class="mt-4">
-                          <h3 class="font-bold text-xl mb-2">Details Information</h3>
-                          <p>It is a long established fact that a reader will be distracted by the readable content of
-                            a page when
-                            looking at its
-                            layout.
-                            The point of using is that it has a more-or-less normal distribution of letters, as
-                            opposed to using.</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="modal-action">
-                    <form method="dialog">
-    
-                      <button class="btn w-full">Close</button>
-                    </form>
-                  </div>
-                </div>
-              </dialog>
-
+              <button class="btn text-md border-[#0E7A81] hover:bg-[#bae8ec] hover:border-[#0E7A81]  transition-all"
+                onclick="loadDetails(${petId})">Details</button>
             </div>
           </div>
         </div>
